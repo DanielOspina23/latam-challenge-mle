59173888540d236575e1e5baa717203ddf02632f
DanielOspina23 <betospina23@hotmail.com>
2025-02-26 17:44:31 -0500

    Build predict logic endpoint and update model endpoint

--- challenge/api.py
+++ challenge/api.py
@@ -3,7 +3,7 @@ import uvicorn
 from fastapi import HTTPException
 
 from challenge.schemas.templates import RequestTemplate, FitRequestTemplate
-from challenge.services.train_service import train_model
+from challenge.services.services import train_model, predict_service, update_model
 from challenge.settings import Settings
 
 settings = Settings()
@@ -15,6 +15,11 @@ app = fastapi.FastAPI(
 )
 
 
+@app.on_event('startup')
+async def startup():
+    update_model()
+
+
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
     return {
@@ -24,8 +29,12 @@ async def get_health() -> dict:
 
 @app.post("/predict", status_code=200)
 async def post_predict(data: RequestTemplate) -> dict:
+    try:
+        predictions = predict_service(data=data.flights)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail='An error occurred during prediction: {str(e)}')
 
-    return {'predict': [0]}
+    return {'predict': predictions}
 
 
 @app.post('/fit', status_code=200)
@@ -36,5 +45,16 @@ async def post_fit(request: FitRequestTemplate) -> dict:
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred during training: {str(e)}")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, loop="asyncio")
+
+@app.get('/update-model', status_code=200)
+async def force_update_model(model_id: str) -> dict:
+    if model_id.endswith('.pkl'):
+        raise HTTPException(status_code=400, detail='Model id should not have extension')
+    try:
+        await update_model(model_name=f'{model_id}.pkl')
+        return {'updated_model': model_id}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'An error occurred during updating model: {str(e)}')
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8080, loop='asyncio')
